5e01b0a14a51beeabface20eaf9b5220ea2cfc91
guicdas <silva.caldeira.guilherme@gmail.com>
2026-04-10 10:49:06 +0100

    added tcs

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -3,20 +3,22 @@
 import { useState } from "react";
 
 export default function AboutUs() {
-	const items: {
+	type faqItem = {
 		title: string;
 		body: string;
-	}[] = [
-			{ title: "What is the role of a mortgage broker in Portugal?", body: "A mortgage broker in Portugal acts as an intermediary between borrowers and lenders. Their role is to help clients secure the best mortgage terms by evaluating their financial situation, comparing loan offers from various banks, and guiding them through the application process." },
-			{ title: "Why should I use a mortgage broker instead of going directly to a bank?", body: "Mortgage brokers offer several advantages. They have access to a wide range of loan products and can provide expert guidance on the best options for your specific needs. Brokers can often secure more favorable terms and help streamline the application process, saving you time and money." },
-			{ title: "How do mortgage brokers get paid in Portugal?", body: "Mortgage brokers in Portugal typically earn a commission from the lending institution when a client's mortgage application is successful. This fee is usually a percentage of the loan amount. Clients generally do not pay brokers directly for their services." },
-			{ title: "Are mortgage brokers regulated in Portugal?", body: "Yes, mortgage brokers in Portugal are regulated by the Bank of Portugal (Banco de Portugal). To operate legally, brokers must hold a valid license issued by this regulatory authority. This ensures that brokers adhere to professional standards and ethical practices." },
-			{ title: "What factors affect mortgage interest rates in Portugal?", body: "Mortgage interest rates in Portugal can be influenced by various factors, including the Eurozone's monetary policy, the property's location, the loan-to-value ratio, the borrower's creditworthiness, and market competition among lenders." },
-			{ title: "How can I improve my chances of getting a mortgage in Portugal?", body: "To increase your likelihood of mortgage approval, maintain a strong credit score, save for a sizable down payment, provide accurate financial documentation, and work with a mortgage broker to identify lenders that match your financial profile." },
-			{ title: "What is the maximum loan-to-value (LTV) ratio for mortgages in Portugal?", body: "The LTV ratio can vary, but it's common for lenders to offer mortgages with an LTV of up to 80% of the property's value. However, specific terms may vary based on your financial situation and the lending institution." },
-			{ title: "Can I get a mortgage in Portugal as a foreign investor or non-resident?", body: "Yes, Portugal welcomes foreign investors, and non-residents can typically secure mortgages for property purchases. The requirements may vary, so it's advisable to work with a mortgage broker experienced in handling international clients." },
-			{ title: "How long does the mortgage approval process in Portugal usually take?", body: "The mortgage approval process can take several weeks in Portugal. It involves property appraisal, credit checks, and documentation verification. Timelines may vary depending on the complexity of the application and the lender's efficiency." },
-		];
+	}
+
+	const items: faqItem[] = [
+		{ title: "What is the role of a mortgage broker in Portugal?", body: "A mortgage broker in Portugal acts as an intermediary between borrowers and lenders. Their role is to help clients secure the best mortgage terms by evaluating their financial situation, comparing loan offers from various banks, and guiding them through the application process." },
+		{ title: "Why should I use a mortgage broker instead of going directly to a bank?", body: "Mortgage brokers offer several advantages. They have access to a wide range of loan products and can provide expert guidance on the best options for your specific needs. Brokers can often secure more favorable terms and help streamline the application process, saving you time and money." },
+		{ title: "How do mortgage brokers get paid in Portugal?", body: "Mortgage brokers in Portugal typically earn a commission from the lending institution when a client's mortgage application is successful. This fee is usually a percentage of the loan amount. Clients generally do not pay brokers directly for their services." },
+		{ title: "Are mortgage brokers regulated in Portugal?", body: "Yes, mortgage brokers in Portugal are regulated by the Bank of Portugal (Banco de Portugal). To operate legally, brokers must hold a valid license issued by this regulatory authority. This ensures that brokers adhere to professional standards and ethical practices." },
+		{ title: "What factors affect mortgage interest rates in Portugal?", body: "Mortgage interest rates in Portugal can be influenced by various factors, including the Eurozone's monetary policy, the property's location, the loan-to-value ratio, the borrower's creditworthiness, and market competition among lenders." },
+		{ title: "How can I improve my chances of getting a mortgage in Portugal?", body: "To increase your likelihood of mortgage approval, maintain a strong credit score, save for a sizable down payment, provide accurate financial documentation, and work with a mortgage broker to identify lenders that match your financial profile." },
+		{ title: "What is the maximum loan-to-value (LTV) ratio for mortgages in Portugal?", body: "The LTV ratio can vary, but it's common for lenders to offer mortgages with an LTV of up to 80% of the property's value. However, specific terms may vary based on your financial situation and the lending institution." },
+		{ title: "Can I get a mortgage in Portugal as a foreign investor or non-resident?", body: "Yes, Portugal welcomes foreign investors, and non-residents can typically secure mortgages for property purchases. The requirements may vary, so it's advisable to work with a mortgage broker experienced in handling international clients." },
+		{ title: "How long does the mortgage approval process in Portugal usually take?", body: "The mortgage approval process can take several weeks in Portugal. It involves property appraisal, credit checks, and documentation verification. Timelines may vary depending on the complexity of the application and the lender's efficiency." },
+	];
 
 	const [openIndex, setOpenIndex] = useState<number | null>(null);
 
@@ -25,7 +27,7 @@ export default function AboutUs() {
 	return (
 		<div style={{
 			width: "100%",
-			backgroundColor: "grey",
+			backgroundColor: "hsl(0 0 70)",
 			height: "fit-content",
 			display: "flex",
 			justifyContent: "center",
@@ -34,8 +36,7 @@ export default function AboutUs() {
 			<div style={{
 				width: "100%",
 				backgroundColor: "hsl(0 0 98)",
-				color: "black",
-
+				color: "black"
 			}}>
 				<div
 					style={{
@@ -46,14 +47,14 @@ export default function AboutUs() {
 						padding: "16px 20px",
 						border: "none",
 						cursor: "pointer",
-						backgroundColor: "hsl(0 0 90)"
+						backgroundColor: "hsl(0 0 85)"
 					}}
 				>
 					FAQ's
 				</div>
-				{items.map((item, i) => (
+				{items.map((item: faqItem, i) => (
 					<div
-						key={i}
+						key={`faq-${i + 1}`}
 						style={{
 							borderBottom: i < items.length - 1 ? "0.5px solid #e0e0e0" : "none"
 						}}
@@ -70,11 +71,12 @@ export default function AboutUs() {
 								border: "none",
 								cursor: "pointer",
 								textAlign: "left",
+								backgroundColor: "hsl(0 0 90)"
 							}}
 						>
 							{item.title}
 							<span style={{
-								transition: "all 100ms ease",
+								transition: "all 200ms ease",
 								translate: openIndex === i ? "0 2px" : "0 -2px",
 								width: "8px",
 								height: "8px",
@@ -85,10 +87,12 @@ export default function AboutUs() {
 						</button>
 
 						<div style={{
-							transition: "all 70ms ease",
+							transition: "all 300ms ease, opacity 70ms ease",
 							opacity: openIndex === i ? "1" : "0",
 							height: openIndex === i ? "fit-content" : "0",
-							padding: "0 20px 16px",
+							paddingTop: openIndex === i ? "20px" : "0",
+							paddingBottom: openIndex === i ? "20px" : "0",
+							paddingLeft: "16px",
 							fontSize: 14,
 							color: "#666"
 						}}>
@@ -98,6 +102,6 @@ export default function AboutUs() {
 					</div>
 				))}
 			</div>
-		</div>
+		</div >
 	);
 }
---
+++ app/TCs/page.tsx
@@ -0,0 +1,75 @@
+export default function TermsAndConditions() {
+    return (
+        <div style={{
+            width: "100vw",
+            backgroundColor: "hsl(0 0 98)",
+            color: "black",
+            height: "fit-content",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    padding: "16px 20px",
+                    border: "none",
+                    backgroundColor: "hsl(0 0 85)"
+                }}
+            >
+                DATA PROTECTION
+            </div>
+
+            <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    border: "none",
+                    backgroundColor: "hsl(0 0 85)",
+                    fontWeight: "bolder",
+                    textAlign: "center",
+                    padding: "0 25vw",
+                }}
+            >
+                The protection of confidentiality and integrity of our customers' data is considered by CPCDS CONSUMER SOLUTIONS as one of the fundamental pillars in the relationship of trust with people who use our services.
+            </div>
+
+            <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    border: "none",
+                    backgroundColor: "hsl(0 0 85)",
+                    fontWeight: "light",
+                    padding: "0 20vw",
+                }}
+            >
+                CPCDS CONSUMER SOLUTIONS has an organization and an adequate process to protect your data against destruction, alteration, and unauthorized access.
+                <br></br>
+                The General Data Protection Regulation (RGPD) applies to the processing of personal data by fully or partially automated means, as well as to the processing by non-automated means of personal data contained in files.
+            </div>
+            <div
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    padding: "0 20vw",
+                    border: "none",
+                    backgroundColor: "hsl(0 0 85)",
+                    fontWeight: "light"
+                }}
+            >
+                We comply with the confidentiality obligations arising from the activity of mortgage and loans brokers under the provisions of applicable national legislation and Regulation (EU) 2016/679 of the European Parliament and of the Council of 27 April 2016 - General Regulation on Data Protection (GDPR).
+            </div>
+        </div>
+    )
+}
